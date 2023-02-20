fda85ec0a7b90937c87788fe6eaedfe4d1b1179e
bca <bca@Bcalab4-m03>
2023-02-20 09:46:14 +0530

    Q1

---
+++ bca124/src/bca124/Car.java
@@ -0,0 +1,18 @@
+package bca124;
+
+public class Car {
+	String model;
+	int speed;
+
+	Car(String model, int speed)
+	{
+		this.model=model;
+		this.speed=speed;
+	}
+		
+	public static void main(String[] args) {
+		
+	}
+
+}
+
